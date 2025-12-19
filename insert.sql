1ab867900329796f0ab49659d2b86d479d5778b1
Joydeep Biswas <joydeep.biswas@capgemini.com>
2025-12-19 09:53:31 +0530

    Create stored procedure to insert car records

--- insert.sql
+++ insert.sql
@@ -1,1 +1,13 @@
-yet to be implement!!
+DELIMITER $$
+
+CREATE PROCEDURE insert_car(
+    IN p_car_id INT,
+    IN p_brand VARCHAR(50),
+    IN p_model VARCHAR(50)
+)
+BEGIN
+    INSERT INTO car (car_id, brand, model)
+    VALUES (p_car_id, p_brand, p_model);
+END$$
+
+DELIMITER ;
